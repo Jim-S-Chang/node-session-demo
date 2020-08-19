258075fee34448a64a7f8977c625f1a85ad00788
Jim-S-Chang <jim.s.chang@qq.com>
2020-08-19 22:47:36 +0800

    [Jim]feature: import express-session and redis but don't use redis

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+let session = require('express-session')
+let RedisStore = require('connect-redis')(session);
+const redis = require('redis');
+const { Store } = require('express-session');
 const app = express()
 
 // 解决跨域
@@ -13,9 +17,36 @@ app.all("*", function(req, res, next) {
         next()
 })
 
+app.use(session({
+    name : "sessionId",
+    secret : 'Jim_secret',
+    resave : false,
+    rolling: true,
+    saveUninitialized : false,
+    cookie : {
+        maxAge: 1000 * 60,
+        httpOnly: true
+    },
+    store : new RedisStore({client: redis.createClient({
+        host: '127.0.0.1',
+        port: 6379
+    })})
+}))
+
+app.use((req, res, next) => {
+    req.session.cookie.maxAge = 1000 * 60
+    next()
+})
+
 app.post('/login', bodyParser.json(), function(req, res) {
-    console.log(req.body)
-    res.send(req.body)
+    console.log(`req.session`, req.session);
+    const userInfo = { name: "Jim", address: "South Park", age: "18" }
+    let expireTime = new Date().getTime() + 60000
+    req.session.save()
+    res.send({
+        userInfo,
+        expireTime: new Date(expireTime).toISOString()
+    })
 })
 
 const server = app.listen(8080, function() {
